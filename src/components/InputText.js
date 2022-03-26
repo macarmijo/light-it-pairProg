97229c2573753b7a1f86a24e906fa827864972e3
Maca <macca.armijo@gmail.com>
2022-03-26 07:59:14 -0700

    corrijo atributos del input y sumo gitignore

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -5,7 +5,9 @@ const InputText = (props) => {
     return (
         <div className="labelInput">
             <label>{props.label}</label>
-            <input name={props.name} type="text"  placeholder={props.helper}></input>
+            <input name={props.name} type="text"  placeholder={props.helper}
+                    value=""
+            />
         </div>
     )
 }
